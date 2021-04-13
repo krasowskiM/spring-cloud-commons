c986b81f918e736c0fdf7a7df6b7a85bc0faa2fe
krasowskiM <krasowski.m87@gmail.com>
2021-04-13 20:15:51 +0200

    Test case for an overriden custom ssl context when using DefaultApacheHttpClientConnectionManagerFactory.newConnectionManager()

--- spring-cloud-commons/src/test/java/org/springframework/cloud/commons/httpclient/DefaultApacheHttpClientConnectionManagerFactoryTests.java
+++ spring-cloud-commons/src/test/java/org/springframework/cloud/commons/httpclient/DefaultApacheHttpClientConnectionManagerFactoryTests.java
@@ -16,25 +16,31 @@
 
 package org.springframework.cloud.commons.httpclient;
 
-import java.lang.reflect.Field;
-import java.util.concurrent.TimeUnit;
-
-import javax.net.ssl.SSLContextSpi;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.X509TrustManager;
-
 import org.apache.http.config.Lookup;
+import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.conn.DefaultHttpClientConnectionOperator;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledForJreRange;
 import org.junit.jupiter.api.condition.JRE;
-
 import org.springframework.util.ReflectionUtils;
 
+import javax.net.ssl.*;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.BDDAssertions.then;
+import static org.springframework.cloud.commons.httpclient.ApacheHttpClientConnectionManagerFactory.HTTPS_SCHEME;
+import static org.springframework.cloud.commons.httpclient.ApacheHttpClientConnectionManagerFactory.HTTP_SCHEME;
 
 /**
  * @author Ryan Baxter
@@ -78,6 +84,36 @@ public class DefaultApacheHttpClientConnectionManagerFactoryTests {
 	}
 
 	@Test
+	public void newConnectionManagerWithCustomSSLContext() {
+		try {
+			//given
+			KeyStore dummyKeystore = KeyStore.getInstance("JKS");
+			dummyKeystore.load(this.getClass().getResourceAsStream("/test_cert.jks"), "123".toCharArray());
+
+			SSLContext customContext = SSLContextBuilder.create()
+					.setProtocol("TLSv1.2")
+					.loadKeyMaterial(dummyKeystore, "123".toCharArray())
+					.build();
+			//when
+			HttpClientConnectionManager connectionManager = new DefaultApacheHttpClientConnectionManagerFactory()
+					.newConnectionManager(false, 2, 6, 10, TimeUnit.SECONDS,
+							RegistryBuilder.create()
+									.register(HTTP_SCHEME, PlainConnectionSocketFactory.INSTANCE)
+									.register(HTTPS_SCHEME, new SSLConnectionSocketFactory(customContext)));
+			//then
+			Lookup<ConnectionSocketFactory> socketFactoryRegistry = getConnectionSocketFactoryLookup(connectionManager);
+			then(socketFactoryRegistry.lookup("https")).isNotNull();
+			then(getX509TrustManager(socketFactoryRegistry).getAcceptedIssuers()).isNotNull();
+
+			X509ExtendedKeyManager x509KeyManager = getX509KeyManager(socketFactoryRegistry);
+			X509ExtendedKeyManager customKeyManager = getCustomKeyManager(customContext);
+			assertThat(x509KeyManager).isEqualTo(customKeyManager);
+		} catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException | CertificateException | IOException | UnrecoverableKeyException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
 	@DisabledForJreRange(min = JRE.JAVA_16)
 	public void newConnectionManagerWithDisabledSSLValidation() {
 		HttpClientConnectionManager connectionManager = new DefaultApacheHttpClientConnectionManagerFactory()
@@ -94,11 +130,25 @@ public class DefaultApacheHttpClientConnectionManagerFactoryTests {
 		return getField(connectionOperator, "socketFactoryRegistry");
 	}
 
-	private X509TrustManager getX509TrustManager(Lookup<ConnectionSocketFactory> socketFactoryRegistry) {
+	private SSLContextSpi getSslContextSpi(Lookup<ConnectionSocketFactory> socketFactoryRegistry) {
 		ConnectionSocketFactory connectionSocketFactory = socketFactoryRegistry.lookup("https");
 		SSLSocketFactory sslSocketFactory = getField(connectionSocketFactory, "socketfactory");
-		SSLContextSpi sslContext = getField(sslSocketFactory, "context");
+		return getField(sslSocketFactory, "context");
+	}
+
+	private X509ExtendedKeyManager getCustomKeyManager(SSLContext sslContext) {
+		SSLContextSpi sslContextSpi = getField(sslContext, "contextSpi");
+		return getField(sslContextSpi, "keyManager");
+	}
+
+	private X509TrustManager getX509TrustManager(Lookup<ConnectionSocketFactory> socketFactoryRegistry) {
+		SSLContextSpi sslContext = getSslContextSpi(socketFactoryRegistry);
 		return getField(sslContext, "trustManager");
+	}
+
+	private X509ExtendedKeyManager getX509KeyManager(Lookup<ConnectionSocketFactory> socketFactoryRegistry) {
+		SSLContextSpi sslContext = getSslContextSpi(socketFactoryRegistry);
+		return getField(sslContext, "keyManager");
 	}
 
 	@SuppressWarnings("unchecked")
